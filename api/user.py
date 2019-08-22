3047f3e4338d334c06c82f73a47a55e6de592cbd
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-08-22 09:46:16 -0600

    regi and login are working

--- api/user.py
+++ api/user.py
@@ -44,16 +44,16 @@ def register():
     pay_file = request.files
     # this has the form info from the dict.
     # we change the request object into a dict.
-    payload = request.form.to.dict()
+    payload = request.form.to_dict()
     dict_file = pay_file.to_dict()
 
     print(payload)
     print(dict_file)
 
-    payload['email'].lower() # make email lowercase
+    payload['username'].lower() # make username lowercase
     try:
-        #check to see if email exisits, if it does let user know. the .get() comes from peeweee
-        models.user.get(models.User.email == payload['email']) # query to find user by their email. if this model is found, responde to client
+        #check to see if email exists, if it does let user know. the .get() comes from peeweee
+        models.User.get(models.User.username == payload['username']) # query to find user by their email. if this model is found, responde to client
         return jsonify(data={}, status={"code": 401, "message": "The user with this name exisits"})
     except models.DoesNotExist: #boolean on the model
         # if model doesnt exisit, we will create/reg this user
@@ -67,7 +67,7 @@ def register():
         payload['image'] = file_picture_path
 
         ## create the row in the table 
-        user = modles.User.create(**payload)
+        user = models.User.create(**payload)
 
         print(type(user)) # => class User user is an instance of 
         login_user(user) #login_user is from flask_login
@@ -82,4 +82,45 @@ def register():
         #remove the password
         del user_dict['password']
         return jsonify(data=user_dict, status={"code": 201, "message": "Success"})
+
+# user login route, Whoop whoop!
+@user.route('/login', methods=["POST"])
+def login():
+    payload = request.get_json()
+    print(payload, '<-- this is the payload')
+    try:
+        user = models.User.get(models.User.username == payload['username'])
+        user_dict = model_to_dict(user)
+        if(check_password_hash(user_dict['password'], payload['password'])):
+            del user_dict['password']
+            login_user(user)
+            print(user, '<-- this is the user')
+            return jsonify(data=user_dict, status={"code": 200, "message": "Success"})
+        else:
+            return jsonify(data={}, status={"code": 401, "message": "Username or Password was incorrect."})
+    except models.DoesNotExist:
+        return jsonify(data={}, status={"code": 401, "message": "Username or Password was incorrect."})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
