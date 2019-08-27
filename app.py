8b461a27dcfeb9a7e9347e47465e8b4519259e9a
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-08-27 13:23:21 -0600

    hopefully working

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_cors import CORS
 from flask_login import LoginManager
 import models
 import os
-#import the blueprint
 from api.user import user
 from api.beer import beer
 
@@ -11,12 +10,11 @@ from api.beer import beer
 DEBUG = True
 PORT = 8000
 
-login_manager = LoginManager() # sets up ability to se up login
+login_manager = LoginManager() 
 
 app = Flask(__name__, static_url_path="", static_folder="static")
-# this is initializing an instance of the Flask class.
 app.secret_key = "RAN@#$%^&*DOM STRING"
-login_manager.init_app(app) #this sets up session on the app
+login_manager.init_app(app) 
 
 @login_manager.user_loader
 def load_user(userid):
@@ -34,13 +32,11 @@ app.register_blueprint(beer)
 
 @app.before_request
 def before_request():
-    """ Connect to the database before each request"""
     g.db = models.DATABASE
     g.db.connect()
 
 @app.after_request
 def after_request(response):
-    """ Close database after each request """
     return response
 
 @app.route('/')
@@ -53,5 +49,4 @@ if 'ON_HEROKU' in os.environ:
     models.initialize()
 
 if __name__ == '__main__':
-    # models.initialize()
     app.run(debug=DEBUG, port=PORT)
